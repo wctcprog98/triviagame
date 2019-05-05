501e201d712ef48853d057e3de7ab6c5795596c3
Nenye <chinenyediei@gmail.com>
2019-05-05 18:21:15 -0500

    Playing with some css grid styles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,62 +6,44 @@ var timerId = setInterval(countdown, 1000);
 //create an array for looping
 
 //create function for onsubmit event looping through array
-$("#answer1, #answer2, #answer3, #answer4").click(function(){
+$("#answer1, #answer2, #answer3, #answer4").click(function () {
 
 
     // for (var i = 0; i < 10; i++)
     // {
-        
 
+    });
     // }
-    
+
     //if butttoncliked = correct button the buttonclicked is true else flase
     //push information to html
-        //correct
-        //wrong
-        //new content
-    
+    //correct
+    //wrong
+    //new content
+
     //timer
-   
-function countdown() {
-    if (timeLeft == -1) {
-        clearTimeout(timerId);
-        doSomething();
-    } else {
-        $("#gameTimer").text(timeLeft + ' seconds remaining';
-        timeLeft--);
+
+    function countdown() {
+        if (timeLeft == -1) {
+            clearTimeout(timerId);
+            doSomething();
+        } else {
+            $("#gameTimer").text(timeLeft + ' seconds remaining'
+
+                +
+                timeLeft--);
+        }
     }
-}
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+countdown(); 
 
-   //create an object for Quiz
-   //answers
-   //question
-   
-    
+    //create an object for Quiz
+    //answers
+    //question
 
-});
+
+
+
 
 //append9 content to html
 
