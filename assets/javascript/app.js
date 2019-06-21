ab13ba6ff0c0b29044dd9ad443aa634ff6c56629
Nenye <chinenyediei@gmail.com>
2019-06-21 04:30:16 -0500

    finally able to pull data from radio buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,11 +3,21 @@ $("#start").on("click", function () {
     for (var i = 0; i < trivia.length; i++) {
         $(".game").append("<h2>" + trivia[i].questions + "</h2>");
         for (var j = 0; j < trivia[i].answers.length; j++) {
-            $(".game").append("<input type= 'radio' name= 'question-" + i + " ' value=' " + trivia[i].answers[j] + " '>" + trivia[i].answers[j])
+            $(".game").append("<input type='radio' name='question" + i + "' value=' " + trivia[i].answers[j] + " '>" + trivia[i].answers[j])
         }
     }
-    
 });
+
+
+//hide start button
+
+
+var answers = [];
+
+$("#start").click(function(){
+    $("#start").hide();
+});
+  
 
     
 var trivia = [{
@@ -33,7 +43,46 @@ var trivia = [{
         correctAnswer: "Rajah"
 
     }];
+ 
+var array = []; 
+
+// var answers = $('input:radio[name=question[i]]:checked').val();
+// console.log(answers); 
     
+// })
+
+
+$('.submit').on('click', function () {
+    answer($('input[name=question0 ]:checked').val());
+    console.log($('input[name=question1 ]:checked').val());
+    console.log($('input[name=question2 ]:checked').val());
+    console.log($('input[name=question3 ]:checked').val());
+    console.log($('input[name=question4 ]:checked').val());
+})
+
+
+
+
+    //start tomer function on click
+
+    //checked = $('input:radio[name=question]:checked').val()
+    //retrieve radio buttons after submit or timer = 0
+
+    //loop throough trivia[0]answers ===playerchoice
+
+    //tell user thier resuls by using $hide/
+
+    //have a button asking if they want to play again
+
+    //on click reset the clock
+
+        //show questions
+
+    
+
+    
+
+
 
 // var game = func
 // correct: 0,
@@ -45,7 +94,7 @@ var trivia = [{
 
  
    
-$("input[name='name_of_your_radiobutton']:checked").val();
+
 // });
     
 // checked = $('input:radio[name=question]:checked').val();
@@ -62,7 +111,13 @@ $("input[name='name_of_your_radiobutton']:checked").val();
 
   
 
-// userAnswer = $("input:checked");
+// userAnswer = $("input:checked var number = 100;
+
+    //  Variable that will hold our interval ID when we execute
+    //  the "run" function
+  
+ 
+    
 // console.log(userAnswer);
 
 
