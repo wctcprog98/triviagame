1a4757479c062abfcbd871041ac68e047b767cdb
Nenye <chinenyediei@gmail.com>
2019-06-30 15:14:51 -0500

    buttons responses are now being tracked correctly and logging to the console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,10 @@
-
 //global variables 
+var correct = 0; 
+var incorrect = 0; 
 
-var answersArray = [];//hold user answers
-var correct = null; 
-var incorrect = null; 
- 
-console.log(total);
-
-
-$("#start").on("click", function () {
-  
-    for (var i = 0; i < trivia.length; i++) {
-        $(".game").append("<h2>" + trivia[i].questions + "</h2>");
-        for (var j = 0; j < trivia[i].answers.length; j++) {
-            $(".game").append("<input type='radio' name='question" + i + "' value=' " + trivia[i].answers[j] + " '>" + trivia[i].answers[j])
-        }
-    }
-});
-
-
-//hide start button
-
-
-
-
-$("#start").click(function(){
-    $("#start").hide();
-});
-  
-
-    
 var trivia = [{
-
     questions: "In Dumbo, where is Mrs. Jumbo when the stork delivers her baby?",
-    answers: ["the circus", "she never did", "the watering hole"],
+    answers: ["the circus", "she never did", "the watering hole", "In the circus train"],
     correctAnswer: "In the circus train"
 }, {
     questions: "In Peter Pan, Captain Hook had a hook on which one of his hands",
@@ -50,120 +21,95 @@ var trivia = [{
 }, {
     questions: "In Aladdin, what is the name of Jasmines pet tiger?",
     answers: ["Tigger", "Shere Khan", "Rajah", "Scar", "Simba"],
-        correctAnswer: "Rajah"
-
+    correctAnswer: "Rajah"
     }];
  
+function startGame() {
+    $("#start").on("click", function () {
+        // hide the start button and create questions and answer radio buttons
+        $("#start").hide();
+        for (var i = 0; i < trivia.length; i++) {
+            $(".game").append("<h2>" + trivia[i].questions + "</h2>");
+            for (var j = 0; j < trivia[i].answers.length; j++) {
+                $(".game").append("<input type='radio' name='question-" + i +
+              "' value='" + trivia[i].answers[j] + "''>" + trivia[i].answers[j]);
+            }
+          }
+          $(".game").append("<button class='submit'>Submit</button>");
+    });
+}
 
-//total
-var total = trivia.length; 
-console.log(total);
-    
-// })
 
+function submitFunction(){
+    console.log("on click submit");
 
-$('.submit').on('click', function () {
-
-
-    //store answers
-    
-    answers = $('input[name=question0]:checked').val(); 
-    answersArray.push(answers);
-    answers1 = ($('input[name=question1]:checked').val());
-    answersArray.push(answers1);
-    answers2 = ($('input[name=question2]:checked').val());
-    answersArray.push(answers2);
-    answers3 = ($('input[name=question3]:checked').val());
-    answersArray.push(answers3);
-    answers4 = ($('input[name=question4]:checked').val());
-    answersArray.push(answers4);
-
-    console.log(answersArray); 
+   
+    $.each($("input[name='question-0']:checked"), function () {
+        //get values of checked radios and compare to correct answer
+        console.log($(this).val().toString() == trivia[0].correctAnswer.toString());
+        console.log(trivia[0].correctAnswer);
+        if ($(this).val() == trivia[0].correctAnswer) {
+            correct++;
+            console.log("correct: " + correct);
+        }
+        else {
+            incorrect++;
+            console.log("incorrect: " + incorrect);
+        }
+    })
+           //get values of checked radios and compare to correct answer
+    $.each($("input[name='question-4']:checked"), function () {
+        if ($(this).val() == trivia[4].correctAnswer) {
+            correct++;
+            console.log("correct: " + correct);
+        }
+        else {
+            incorrect++;
+            console.log("incorrect: " + incorrect);
+        }
+    })
+           //get values of checked radios and compare to correct answer
+    $.each($("input[name='question-1']:checked"), function () {
+        if ($(this).val() == trivia[1].correctAnswer) {
+            correct++;
+            console.log("correct: " + correct);
+        }
+        else {
+            incorrect++;
+            console.log("incorrect: " + incorrect);
+        }
+    })
+           //get values of checked radios and compare to correct answer
+    $.each($("input[name='question-2']:checked"), function () {
+        if ($(this).val() == trivia[2].correctAnswer) {
+            correct++;
+            console.log("correct: " + correct);
+        }
+        else {
+            incorrect++;
+            console.log("incorrect: " + incorrect);
+        }
+    })
+           //get values of checked radios and compare to correct answer
+    $.each($("input[name='question-3']:checked"), function () {
+        if ($(this).val() == trivia[3].correctAnswer) {
+            correct++;
+            console.log("correct: " + correct);
+        }
+        else {
+            incorrect++;
+            console.log("incorrect: " + incorrect);
+        }
+    })
+    showResults();
+}; 
+function showResults() {
+   //displays total correct and incorrect answers from quiz
+    console.log("correct: " + correct);
+    console.log("incorrect: " + incorrect);
+}
+$(document).on("click",".submit",function(){
+    submitFunction();
 })
 
-//check answers
-//create for loop checking correctAnswer 
-if (answersArray[i] == trivia[i].correctAnswer)
-{
-    
-    }
-
-
-
-//display results
-//hide/show
-
-
-    //start tomer function on click
-
-    //checked = $('input:radio[name=question]:checked').val()
-    //retrieve radio buttons after submit or timer = 0
-
-    //loop throough trivia[0]answers ===playerchoice
-
-    //tell user thier resuls by using $hide/
-
-    //have a button asking if they want to play again
-
-    //on click reset the clock
-
-        //show questions
-
-    
-
-    
-
-
-
-// var game = func
-// correct: 0,
-//     incorrect: 0; 
-// counter: 120;
-// countdown: function() {
-//     game.counter
-// }
-
- 
-   
-
-// });
-    
-// checked = $('input:radio[name=question]:checked').val();
-// console.log(checked); 
-//     // $("#rad").click(function(){
-    //     var $ctrl = $('<input/>').attr({ type: 'radio', name:'rad'}).addClass("rad");
-    //     $("#holder").append($ctrl);
-
-
-    // $("input[type='radio']").click(function () {
-     
-
-      
-
-  
-
-// userAnswer = $("input:checked var number = 100;
-
-    //  Variable that will hold our interval ID when we execute
-    //  the "run" function
-  
- 
-    
-// console.log(userAnswer);
-
-
-
-
-
-// $(window).load(function () {
-//     // Run code
-//     $(".game").hide();
-// });
-
-
-
-
-
-
-
-
+startGame(); 
