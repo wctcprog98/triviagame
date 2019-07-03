610bcc1808efe535f58409b6ea716efe79c2c6ac
Nenye <chinenyediei@gmail.com>
2019-07-03 01:21:46 -0500

    Timer is working and results show on screen when timer runs out or when submit button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,6 +27,7 @@ var trivia = [{
 function startGame() {
     $("#start").on("click", function () {
         // hide the start button and create questions and answer radio buttons
+        timer(); 
         $("#start").hide();
         $(".blinking").hide();
 
@@ -109,6 +110,7 @@ function showResults() {
 
     //hide current screen 
     $(".game").hide();
+    $(".timer-wrap").hide(); 
     if (correct > 1)
     {
         $(".results").append("<h2>" + "you got " + correct + " answers correct" + "</h2>"); 
@@ -127,9 +129,63 @@ function showResults() {
 
     }
 
+    //button to restart game
+    var button = $("<button>"); 
+    
+  
 }
 $(document).on("click",".submit",function(){
     submitFunction();
 })
 
-startGame(); 
+
+function timer() {
+    $(document).ready(function () {
+
+        var time = $(".time").text();
+        var interval = setInterval(function () {
+            var count = time.split(":");
+            var hour = count[0];
+            var min = count[1];
+            var sec = count[2];
+            var counter = $(".timer").find("span");
+            counter.eq(0).text(hour);
+            counter.eq(1).text(min);
+            counter.eq(2).text(sec);
+
+            if (hour == 0 && min == 0 && sec <= 30) {
+                $(".timer").css("color", "red");
+            }
+            if (sec == 0) {
+                if (min != 0) {
+                    counter.eq(1).text(min--);
+                    sec = 59;
+                }
+                else if (min == 0 && hour != 0) {
+                    counter.eq(0).text(hour--);
+                    counter.eq(1).text(min = 59);
+                    sec = 59;
+                }
+                else if (hour == 0 && min == 0 && sec == 0) {
+                    counter.eq(2).text(0);
+                }
+                if (hour == 0 && min == 0) {
+                    $(".timer").css("color", "red");
+                
+                }
+                
+            }
+            counter.eq(0).text(hour);
+            counter.eq(1).text(min);
+            counter.eq(2).text(--sec);
+            time = counter.eq(0).text() + ":" + counter.eq(1).text() + ":" + counter.eq(2).text();
+            if (hour == 0 && min == 0 && sec == 0) {
+                clearInterval(interval);
+                showResults(); 
+            }
+        }, 1000);
+    });
+}
+
+
+startGame();    
